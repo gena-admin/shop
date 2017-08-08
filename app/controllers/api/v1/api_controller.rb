4b5710140a85186848dd1d0e83b3c036caf4dea7
Gennadiy Zelenskyi <g.zelenskyi@svitla.com>
2017-08-08 22:47:50 +0300

    Added test with AUTH_token

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,11 +1,13 @@
 class Api::V1::ApiController < ActionController::Base
-  # around_action :verify_authenticity_token
-
+  around_action :verify_authenticity_token
 
   private
 
   def verify_authenticity_token
-    if headers['GENA_AUTH_TOKEN'] != 'TEST'
+    # In a general way we should use AUTH token which related to each
+    # user(we should store it in DB) etc. But now I am using one token for site.
+    binding.pry
+    if headers['SHOP_AUTH_TOKEN'] == 'MY_AUTH'
       yield
     else
       render json: { message: 'Please provide valid api token'}, status: :unauthorized
